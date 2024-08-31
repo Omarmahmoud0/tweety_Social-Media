c89f8f182fab89d950487f4266d3327b583fb4a4
Omarmahmoud0 <omarmahmoud@gmail.com>
2024-08-31 21:47:30 +0300

    added App.jsx file

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,12 +1,9 @@
 import Bottombar from "@/components/shared/Bottombar";
 import Leftbar from "@/components/shared/Leftbar";
 import Topbar from "@/components/shared/Topbar";
-import { useUserContext } from "@/context/AuthContext";
 import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
-  const { user } = useUserContext();
-
   return (
     <div className="w-full md:flex">
       <Topbar />
